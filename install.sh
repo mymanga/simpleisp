8f45526813c68e0ec8521eea8de4c0204e1ffcb9
Barnabie Opiyo <1921395+mymanga@users.noreply.github.com>
2025-02-23 08:05:13 +0300

    NGINX fixes

--- install.sh
+++ install.sh
@@ -166,36 +166,34 @@ chmod +x openvpn.sh
 cat > /etc/nginx/sites-available/default << EOL
 server {
     listen 80;
-    server_name _;
+    listen [::]:80;
+
     root /var/www/html/public;
+    index index.php index.html index.htm index.nginx-debian.html;
 
-    add_header X-Frame-Options "SAMEORIGIN";
-    add_header X-Content-Type-Options "nosniff";
-
-    index index.php;
-
-    charset utf-8;
+    server_name $DOMAIN;
 
     location / {
         try_files \$uri \$uri/ /index.php?\$query_string;
     }
 
-    location = /favicon.ico { access_log off; log_not_found off; }
-    location = /robots.txt  { access_log off; log_not_found off; }
-
-    error_page 404 /index.php;
-
     location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
-        include fastcgi_params;
     }
 
-    location ~ /\.(?!well-known).* {
+    location ~ /\.ht {
         deny all;
     }
 }
 EOL
+
+# Test the Nginx configuration
+nginx -t
+
+# If the configuration is OK, reload and restart Nginx
+systemctl reload nginx
+systemctl restart nginx
 
 # Set correct permissions
 chown -R www-data:www-data /var/www/html
